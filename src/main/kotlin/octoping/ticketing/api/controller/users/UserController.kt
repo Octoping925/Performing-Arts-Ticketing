eeb42789b5eda969d850fb0db343db824928743f
Octoping <mooner925@gmail.com>
2025-02-01 21:08:56 +0900

    feat: User 회원가입 구현

--- src/main/kotlin/octoping/ticketing/api/controller/users/UserController.kt
+++ src/main/kotlin/octoping/ticketing/api/controller/users/UserController.kt
@@ -1,12 +1,19 @@
 package octoping.ticketing.api.controller.users
 
-import org.springframework.web.bind.annotation.PostMapping
+import octoping.ticketing.domain.users.service.UserService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController {
-    @PostMapping("/users")
-    fun createUser() {
-        println("User created")
+class UserController(
+    private val userService: UserService,
+) {
+    // fixme: 테스트 편의를 위해 그냥 GET으로 구현
+    @GetMapping("/users")
+    fun createUser(
+        @RequestParam username: String,
+    ) {
+        userService.createUser(username)
     }
 }
